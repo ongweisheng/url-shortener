8a0f7bfbc1b42352faeac320948ded4d2445677c
ongweisheng <ongweisheng30@gmail.com>
2022-09-21 21:51:55 +0800

    Add GET api to get originalUrl from short

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -25,11 +25,24 @@ export async function getUrls(req, res) {
 
 export async function deleteUrl(req, res) {
     try {
-        const response = await Url.deleteOne(req.body)
+        const response = await Url.deleteOne(req.body) // delete by originalUrl or shortenedUrl
         if (response.deletedCount === 0) {
             throw ({ status: 404, message: "Error! Url cannot be deleted or has already been deleted!" })
         } else {
             return res.status(200).json(response)
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+export async function getOriginalUrl(req, res) {
+    try {
+        const shortenedUrl = await Url.findOne({ shortenedUrl: req.params.shortenedUrl })
+        if (shortenedUrl === null) {
+            throw ({ status: 404, message: "Error! Shortened url cannot be found!" })
+        } else {
+            res.redirect(shortenedUrl.originalUrl)
         }
     } catch (err) {
         res.status(500).json(err)
--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route("/").post(urlController.createShortenedUrl)
 router.route("/").get(urlController.getUrls)
 router.route("/").delete(urlController.deleteUrl)
+router.route("/:shortenedUrl").get(urlController.getOriginalUrl)
 
 export default router
