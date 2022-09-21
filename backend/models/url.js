8afb3aeaf4b461a5722ca784106e05e88e4192c6
ongweisheng <ongweisheng30@gmail.com>
2022-09-21 20:52:17 +0800

    Create model/schema

---
+++ backend/models/url.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { nanoid } from 'nanoid'
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+    },
+    shortenedUrl: {
+        type: String,
+        required: true,
+        default: nanoid()
+    }
+})
+
+export default mongoose.model("Url", urlSchema)
