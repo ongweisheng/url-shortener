8d402180121c79577fa588f858080eda7f01e234
ongweisheng <ongweisheng30@gmail.com>
2022-09-22 21:07:35 +0800

    Add UrlService for backend API to be called

---
+++ frontend/src/services/UrlService.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+const baseUrl = "https://mighty-sands-84969.herokuapp.com/urlShortener"
+
+const createShortenedUrl = async (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    const response = await request
+    return response.data
+}
+
+const getUrls = async () => {
+    const request = axios.get(baseUrl)
+    const response = await request
+    return response.data
+}
+
+const deleteUrl = async (newObject) => {
+    const request = axios.delete(baseUrl, newObject)
+    const response = await request
+    return response.data
+}
+
+const getOriginalUrl = async (shortenedUrl) => {
+    const request = axios.get(`${baseUrl}/${shortenedUrl}`)
+    const response = await request
+    return response.data
+}
+
+export default { createShortenedUrl, getUrls, deleteUrl, getOriginalUrl }
