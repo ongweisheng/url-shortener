9df07a38aca8f9242a88f6ea7f1e3c83d32bd2dd
ongweisheng <ongweisheng30@gmail.com>
2022-09-22 21:32:53 +0800

    Add create shortened url frontend component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CreateShortenedUrl from "./components/CreateShortenedUrl.js"
 
 function App() {
   return (
     <div className="container">
-      Hello world
+      <h1 className="mb-5">Url Shortening Service</h1>
+      <CreateShortenedUrl />
     </div>
   );
 }
